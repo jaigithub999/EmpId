f36f5bf81b17d50c97973b10ea9ef45a70c32c35
jaigithub999 <jai.jayaselvam@gmail.com>
2023-05-26 17:47:26 +0530

    vanakam da mapla thenila irunthu

--- src/test/java/org/sample/SamplePro.java
+++ src/test/java/org/sample/SamplePro.java
@@ -5,10 +5,15 @@ public class SamplePro {
 	private void empid(int Id) {
 		System.out.println("Emp id: "+Id);
 	}
+	private void hi() {
+		
+		System.out.println("Hello every one");
+		}
 	public static void main(String[] args) {
 		System.out.println("Github push");
 		SamplePro s = new SamplePro();
 		s.empid(1234);
+		s.hi();
 	}
 
 }
