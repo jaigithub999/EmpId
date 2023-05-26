e11ba32274cebbe3e3ae5adf826eb9d1c539bf9d
jaigithub999 <jai.jayaselvam@gmail.com>
2023-05-26 17:57:14 +0530

    Thi is created by Jayaselvam not wolverine

--- src/test/java/org/sample/SamplePro.java
+++ src/test/java/org/sample/SamplePro.java
@@ -5,6 +5,10 @@ public class SamplePro {
 	private void empid(int Id) {
 		System.out.println("Emp id: "+Id);
 	}
+	
+	private void moneyInTheBank() {
+		System.out.println("Money in the Bank Is On WWE Game");
+	}
 	public static void main(String[] args) {
 		System.out.println("Github push");
 		SamplePro s = new SamplePro();
